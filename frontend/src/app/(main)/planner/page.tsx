b29667b97e2007760af2267ed822577306e3f43d
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-10-24 21:09:19 -0700

    feat(UI / func): add planner page for chatbot

---
+++ frontend/src/app/(main)/planner/page.tsx
@@ -0,0 +1,9 @@
+import ChatContainer from "@/components/Planner/ChatContainer";
+
+export default function Planner() {
+    return (
+        <div className="flex justify-center p-8">
+        <ChatContainer />
+        </div>
+    )
+}
